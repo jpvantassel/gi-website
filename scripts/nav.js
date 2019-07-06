c83b809fde6ba0ec75edc9a0a93058782d952732
Joseph vantassel <jvantassel@utexas.edu>
2019-07-06 17:55:19 -0500

    :racehorse: Set photo to jpg for speed :lipstick: update nav

---
+++ scripts/nav.js
@@ -0,0 +1,14 @@
+// Written by: Joseph Vantassel 
+// Written on: 6/29/2019
+
+console.log("Nav Tada!")
+document.getElementById("nav-nav").innerHTML = `
+<ul>
+    <li class="current"><a href="index.html">Home</a></li>
+    <li><a href="seminars.html">Educational Seminars</a></li>
+    <li><a href="social.html">Social Events</a></li>
+    <li><a href="educational.html">Educational Resources</a></li>
+    <li><a href="courses.html">Academic Resources</a></li>
+    <li><a href="officers.html">Officers</a></li>
+</ul>
+`
